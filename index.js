4ed75530ce715eca15f3c698b396c092f0b6b294
Vahid Farjood <vahid.farjood@gmail.com>
2023-09-17 17:44:52 +0200

    update index for https

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-Vision For Computer
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Before the script...</p>
+
+  <script>
+    <script src="https://visionforcomputer.com/index.js"></script>
+  </script>
+
+  <p>...After the script.</p>
+
+</body>
+
+</html>
+
+
+
