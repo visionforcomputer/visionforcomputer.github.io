c171799e73d77d44ba688ba7f1a97e2ec8999687
Vahid Farjood <vahid.farjood@gmail.com>
2023-09-17 18:08:42 +0200

    update index.js

--- index.js
+++ index.js
@@ -3,13 +3,9 @@
 
 <body>
 
-  <p>Before the script...</p>
-
   <script>
-    <script src="https://visionforcomputer.com/index.js"></script>
+    <script src="Vision For Computer"></script>
   </script>
-
-  <p>...After the script.</p>
 
 </body>
 
